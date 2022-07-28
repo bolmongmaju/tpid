9816e9edd1c214480f8937725c88aac07522354e
IndraSatya94 <minsatya94@gmail.com>
2022-07-28 15:03:53 +0800

    add tupoksi

--- resources/views/opd/detail/tupoksi.blade.php
+++ resources/views/opd/detail/tupoksi.blade.php
@@ -132,48 +132,33 @@
          <ol>
            <li><a href="index.html">Home</a></li>
          </ol>
-         <h2>Gambar</h2>
+         <h2>Tupoksi</h2>
  
        </div>
      </section><!-- End Breadcrumbs -->
 
          <!-- ======= About Us Section ======= -->
-<!-- ======= Portfoio Section ======= -->
-<section id="portfolio" class="portfoio">
-  <div class="container" data-aos="fade-up">
+    <section id="about" class="about">
+      <div class="container" data-aos="fade-up">
 
-    <div class="section-title">
-      <h2>Gambar</h2>
-      <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
-    </div>
-
-    <div class="row">
-      <div class="col-lg-12 d-flex justify-content-center">
-        <ul id="portfolio-flters">
-          <li data-filter="*" class="filter-active">All</li>
-          <li data-filter=".filter-app">App</li>
-          <li data-filter=".filter-card">Card</li>
-          <li data-filter=".filter-web">Web</li>
-        </ul>
-      </div>
-    </div>
-
-    <div class="row portfolio-container">
-
-      <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-        <img src="assets/img/portfolio/portfolio-1.jpg" class="img-fluid" alt="">
-        <div class="portfolio-info">
-          <h4>App 1</h4>
-          <p>App</p>
-          <a href="assets/img/portfolio/portfolio-1.jpg" data-gallery="portfolioGallery" class="portfolio-lightbox preview-link" title="App 1"><i class="bx bx-plus"></i></a>
-          <a href="portfolio-details.html" class="details-link" title="More Details"><i class="bx bx-link"></i></a>
+        <div class="section-title">
+          <h2>tupoksi</h2>
         </div>
+
+        <div class="row content">
+          <div class="col-lg-12">
+            @forelse($visimisi as $item)
+            <p>
+              {!! nl2br(($item->visi)) !!}
+            </p>
+            @empty
+              Belum ada data
+            @endforelse
+          </div>
+        </div>
+
       </div>
-
-    </div>
-
-  </div>
-</section><!-- End Portfoio Section -->
+    </section><!-- End About Us Section -->
  
     
    </main><!-- End #main -->
